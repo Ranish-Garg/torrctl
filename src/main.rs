66bfac55f33cb1141f9aa7aafb191bc716eb1df8
Ranish-Garg <ranishgarg2006@gmail.com>
2026-03-31 00:00:45 +0530

    feat:decode bencoded strings and integers

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+use std::{env, f32::consts::E};
+
+// Available if you need it!
+// use serde_bencode
+
+#[allow(dead_code)]
+fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+    // If encoded_value starts with a digit, it's a number
+    if encoded_value.chars().next().unwrap()=='i' && encoded_value.chars().last().unwrap()=='e'
+    {
+        let number_str = &encoded_value[1..encoded_value.len()-1];
+   
+        let num: i64 = number_str.parse().unwrap();
+        let json_number = serde_json::Number::from(num);  
+        serde_json::Value::Number(json_number)
+    }
+
+    else if encoded_value.chars().next().unwrap().is_ascii_digit() {
+        // Example: "5:hello" -> "hello"
+        let colon_index = encoded_value.find(':').unwrap();
+        let number_string = &encoded_value[..colon_index];
+        let number = number_string.parse::<usize>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
+        serde_json::Value::String(string.to_string())
+    } else {
+        panic!("Unhandled encoded value: {}", encoded_value)
+    }
+}
+
+// Usage: your_program.sh decode "<encoded_value>"
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let command = &args[1];
+
+    if command == "decode" {
+        // You can use print statements as follows for debugging, they'll be visible when running tests.
+        eprintln!("Logs from your program will appear here!");
+
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
+    } else {
+        println!("unknown command: {}", args[1])
+    }
+}
